397e4dab2bcac24ff44e80a6bf54432601451f78
yabes123 <46999948+yabes123@users.noreply.github.com>
2022-10-28 19:36:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,15 +14,15 @@ global.APIs = {
 
 // Apikey Website Api
 global.APIKeys = {
-	'https://zenzapis.xyz': 'Your Key',
+	'https://zenzapis.xyz': 'sonelstore',
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.owner = ['089522720791','6289512545999']
+global.premium = ['089522720791']
+global.packname = 'Danzz Coding'
+global.author = 'https://danzzapi.xyz'
+global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
